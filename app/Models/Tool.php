5dcb531552f830c52528c7515643264413769427
abdobargush <zezo.mohamed00@gmail.com>
2021-05-04 22:40:14 +0200

    Move storage from azure to s3

--- app/Models/Tool.php
+++ app/Models/Tool.php
@@ -9,102 +9,99 @@ use Intervention\Image\ImageManagerStatic as Image;
 
 class Tool extends Model
 {
-    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
+  use \Backpack\CRUD\app\Models\Traits\CrudTrait;
 
-    /**
-     * Fillable attributes
-     *
-     * @var array
-     */
-    protected $fillable = ['title', 'thumbnail'];
-
-
-    /**
-     * Hidden attributes
-     *
-     * @var array
-     */
-    protected $hidden = ['created_at', 'updated_at'];
+  /**
+   * Fillable attributes
+   *
+   * @var array
+   */
+  protected $fillable = ['title', 'thumbnail'];
 
 
-    /**
-     * Set thumbnail mutator
-     *
-     * Generate and set the tumbnail image of the tool
-     *
-     * @param string $value
-     * @return void
-     **/
-    public function setThumbnailAttribute($value)
-    {
-        $attribute_name = "thumbnail";
-        $disk = config('filesystems.cloud') ?? 'public';
-        // destination path relative to the disk above
-        $destination_path = "uploads/tools";
+  /**
+   * Hidden attributes
+   *
+   * @var array
+   */
+  protected $hidden = ['created_at', 'updated_at'];
 
-        // if the image was erased
-        if ($value == null) {
-            // delete the image from disk
-            Storage::disk($disk)->delete($this->getRawOriginal($attribute_name));
 
-            // set null in the database column
-            $this->attributes[$attribute_name] = null;
-        }
+  /**
+   * Set thumbnail mutator
+   *
+   * Generate and set the tumbnail image of the tool
+   *
+   * @param string $value
+   * @return void
+   **/
+  public function setThumbnailAttribute($value)
+  {
+    $attribute_name = "thumbnail";
 
-        // if a base64 was sent, store its path in the db
-        if (Str::startsWith($value, 'data:image')) {
-            // 0. Make the image
-            $image = Image::make($value)->encode('png')->fit(256, 256);
+    // destination path relative to the disk above
+    $destination_path = "tools";
 
-            // 1. Generate a filename.
-            $filename = md5($value . time()) . '.png';
+    // if the image was erased
+    if ($value == null) {
+      // delete the image from disk
+      Storage::delete($this->getRawOriginal($attribute_name));
 
-            // 2. Store the image on disk.
-            Storage::disk($disk)->put("{$destination_path}/{$filename}", $image->stream());
-
-            // 3. Delete the previous image, if there was one.
-            Storage::disk($disk)->delete("{$destination_path}/" . $this->getRawOriginal($attribute_name));
-
-            // 4. Save the path to the database
-            $this->attributes[$attribute_name] = "{$destination_path}/{$filename}";
-        }
+      // set null in the database column
+      $this->attributes[$attribute_name] = null;
     }
 
+    // if a base64 was sent, store its path in the db
+    if (Str::startsWith($value, 'data:image')) {
+      // 0. Make the image
+      $image = Image::make($value)->encode('png')->fit(256, 256);
 
-    /**
-     * Get thumbnail mutator
-     *
-     * Return tool thumbnail from cloud or public storage
-     * and default tool thumbnail if it doesn't exist
-     * 
-     * @param string $value
-     * @return string
-     **/
-    public function getThumbnailAttribute($value)
-    {
-        // Return default thumbnail if not set
-        if (!$value) return asset('images/tool-default@2x.png');
+      // 1. Generate a filename.
+      $filename = md5($value . time()) . '.png';
 
-        // Return value if it's a url
-        if (substr($value, 0, 4) === 'http') return $value;
+      // 2. Store the image on disk.
+      Storage::put("{$destination_path}/{$filename}", $image->stream(), 'public');
 
-        // Return from cloud if set
-        if (config('filesystems.cloud')) return Storage::cloud()->url($value);
+      // 3. Delete the previous image, if there was one.
+      Storage::delete("{$destination_path}/" . $this->getRawOriginal($attribute_name));
 
-        // Return from public storage if no cloud storage
-        return Storage::disk('public')->url($value);
+      // 4. Save the path to the database
+      $this->attributes[$attribute_name] = "{$destination_path}/{$filename}";
     }
+  }
 
 
-    /**
-     * 1-n relationship
-     * 
-     * The tutorials belong to this tool
-     *
-     * @return object
-     */
-    public function tutorials()
-    {
-        return $this->belongsToMany(Tutorial::class);
-    }
+  /**
+   * Get thumbnail mutator
+   *
+   * Return tool thumbnail from cloud or public storage
+   * and default tool thumbnail if it doesn't exist
+   * 
+   * @param string $value
+   * @return string
+   **/
+  public function getThumbnailAttribute($value)
+  {
+    // Return default thumbnail if not set
+    if (!$value) return asset('images/tool-default@2x.png');
+
+    // Return value if it's a url
+    if (substr($value, 0, 4) === 'http') return $value;
+
+    // Return from storage
+    return Storage::url($value);
+  }
+
+
+  /**
+   * 1-n relationship
+   * 
+   * The tutorials belong to this tool
+   *
+   * @return object
+   */
+  public function tutorials()
+  {
+    return $this->belongsToMany(Tutorial::class);
+  }
 }
--- config/filesystems.php
+++ config/filesystems.php
@@ -2,7 +2,7 @@
 
 return [
 
-    /*
+  /*
     |--------------------------------------------------------------------------
     | Default Filesystem Disk
     |--------------------------------------------------------------------------
@@ -13,9 +13,9 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+  'default' => env('FILESYSTEM_DRIVER', 'local'),
 
-    /*
+  /*
     |--------------------------------------------------------------------------
     | Default Cloud Filesystem Disk
     |--------------------------------------------------------------------------
@@ -26,9 +26,9 @@ return [
     |
     */
 
-    'cloud' => env('FILESYSTEM_CLOUD'),
+  'cloud' => env('FILESYSTEM_CLOUD'),
 
-    /*
+  /*
     |--------------------------------------------------------------------------
     | Filesystem Disks
     |--------------------------------------------------------------------------
@@ -41,42 +41,33 @@ return [
     |
     */
 
-    'disks' => [
+  'disks' => [
 
-        'local' => [
-            'driver' => 'local',
-            'root' => storage_path('app'),
-        ],
-
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
-            'visibility' => 'public',
-        ],
-
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-        ],
-
-        'azure' => [
-            'driver'    => 'azure',
-            'name'      => env('AZURE_STORAGE_NAME'),
-            'key'       => env('AZURE_STORAGE_KEY'),
-            'container' => env('AZURE_STORAGE_CONTAINER'),
-            'url'       => env('AZURE_STORAGE_URL'),
-            'prefix'    => null,
-        ],
-
+    'local' => [
+      'driver' => 'local',
+      'root' => storage_path('app'),
     ],
 
-    /*
+    'public' => [
+      'driver' => 'local',
+      'root' => storage_path('app/public'),
+      'url' => env('APP_URL') . '/storage',
+      'visibility' => 'public',
+    ],
+
+    's3' => [
+      'driver' => 's3',
+      'key' => env('AWS_ACCESS_KEY_ID'),
+      'secret' => env('AWS_SECRET_ACCESS_KEY'),
+      'region' => env('AWS_DEFAULT_REGION'),
+      'bucket' => env('AWS_BUCKET'),
+      'url' => env('AWS_URL'),
+      'endpoint' => env('AWS_ENDPOINT'),
+    ],
+
+  ],
+
+  /*
     |--------------------------------------------------------------------------
     | Symbolic Links
     |--------------------------------------------------------------------------
@@ -87,8 +78,8 @@ return [
     |
     */
 
-    'links' => [
-        public_path('storage') => storage_path('app/public'),
-    ],
+  'links' => [
+    public_path('storage') => storage_path('app/public'),
+  ],
 
 ];
